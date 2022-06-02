768d12dc212b64e8d1192f816e0e4d13039e2faa
Dagmawi.Dedjene <alphadagmawi@gmail.com>
2022-06-02 14:39:58 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * mai- entry point
+ * main- entry point
  *
  * Return: always 0 (sucess)
  */
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
  * main- entry point
  *
- * return: always 0 (success)
+ * retrun: always 0 (success)
  */
 int main(void)
 {
