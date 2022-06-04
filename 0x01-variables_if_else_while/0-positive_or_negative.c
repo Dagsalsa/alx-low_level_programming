cc21a72bef26c15c8812f063b78d4fde936341bc
Dagmawi.Dedjene <alphadagmawi@gmail.com>
2022-06-04 13:08:35 -0700

    h

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@
   n = rand() - RAND_MAX /2;
   if (n > 0)
   {
-  printf("%n : is positive\n");
+  printf("%d : is positive\n","n");
   }
   else
   {
-  printf("%n : is negative\n");
+  printf("%d : is negative\n","n");
   }
   return (0);
   }
