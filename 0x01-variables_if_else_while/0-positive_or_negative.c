391d98532bdcab48ec333d3a596e1088f82b6d64
Dagmawi.Dedjene <alphadagmawi@gmail.com>
2022-06-04 00:16:56 -0700

    s

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,18 +8,18 @@
  * Description: tell if a number is positive or negative
  *
  * Return: Always 0 (Success)
- * */
- * int main(void)
- * {
- * srand(time(0));
- * n = rand() - RAND_MAX /2;
- * if (n>0)
- * {
- * printf("%n : is positive\n");
- * }
- * else
- * {
- * printf("%n : is negative\n");
- * }
- * return (0);
- * }
+ **/
+ int main(void)
+  {
+  srand(time(0));
+  n = rand() - RAND_MAX /2;
+  if (n>0)
+  {
+  printf("%n : is positive\n");
+  }
+  else
+  {
+  printf("%n : is negative\n");
+  }
+  return (0);
+  }
