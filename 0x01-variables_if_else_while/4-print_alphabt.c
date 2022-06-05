caa74f74e5d22c07167a4ca6da32a54a7d48e65e
Dagmawi.Dedjene <alphadagmawi@gmail.com>
2022-06-05 13:43:58 -0700

    s

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdib.h>
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char al;
+for (al = 'a'; al <= 'z'; al++)
+{
+if (al == 'q' || al == 'e')
+{
+continue;
+}
+putchar(al);
+}
+putchar('\n');
+return (0);
+}
