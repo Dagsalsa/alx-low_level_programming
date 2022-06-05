d35c184393ccc7c95a4a032e0d7283e936a38384
Dagmawi.Dedjene <alphadagmawi@gmail.com>
2022-06-05 14:11:00 -0700

    s

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
