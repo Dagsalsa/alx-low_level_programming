fd9b2b40a299d15673efed9a337db1812c5df32e
Dagmawi.Dedjene <alphadagmawi@gmail.com>
2022-06-06 11:42:07 -0700

    s

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+char i;
+char j;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; i <= '9'; i++)
+{
+putchar(ij)
+}
+}
+putchar('\n');
+return (0);
+}
