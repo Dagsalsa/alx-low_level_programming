73f7330dfff9cff11ea3110dc329e017690e1207
Dagmawi.Dedjene <alphadagmawi@gmail.com>
2022-06-07 12:46:42 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
 void print_alphabet(void)
 {
 char alph;
-for (alph = 'a'; alph <= 'z'; alpha++)
+for (alph = 'a'; alph <= 'z'; alph++)
 {
 putchar(alph);
 }
 putchar('\n');
 }
-}
+
